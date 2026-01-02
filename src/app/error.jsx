352e40eeb9a5b40ea7b79c32a9a94bdb2dcf01fd
ANTU SHIL <anturoy084@gmail.com>
2026-01-02 20:11:54 +0600

    added error and loading

---
+++ src/app/error.jsx
@@ -0,0 +1,17 @@
+"use client"
+import Link from 'next/link';
+import React from 'react';
+import { BiSolidErrorAlt } from 'react-icons/bi';
+
+const error = () => {
+    return (
+        <div className='flex flex-col min-h-screen justify-center items-center gap-6'>
+            <BiSolidErrorAlt size={100} className='text-primary' />
+            <h2 className='text-4xl font-bold'>Page Not Found</h2>
+            <Link className='btn btn-primary btn-outline ' href={"/"}>Got to Home</Link>
+            
+        </div>
+    );
+};
+
+export default error;
